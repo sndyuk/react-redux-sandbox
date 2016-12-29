0fb53a2cb767e045f33d0293b839520988a3127f
Yuki Sanada <sanada@sndyuk.com>
2016-12-29 22:06:26 +0000

    Add the line for Redux devtool

--- src/script/index.jsx
+++ src/script/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import App from './component/App';
 import reducer from './reducer';
 
 import '../style/app.scss';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+  // FIXME Omit the line when it's on production.
+  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line no-underscore-dangle, max-len, space-infix-ops
+);
 
 render(
   <Provider store={store}>
